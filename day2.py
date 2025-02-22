53d0d563996c600fc3017bb326d1795ecfbcd1b6
revanth kallakuri <revanthkallakuri@gmail.com>
2025-02-22 12:22:43 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,50 @@
+# # task1
+
+# for i in range(1,101):
+#     print(i)
+
+# #task2
+
+# n=5
+# sum=(n*(n+2))/2
+# print(sum)
+
+
+# #task3
+
+# num=1
+# while(num<=50):
+#     print(num)
+#     num+=1
+    
+# #task4
+
+
+# number=int(input("enter a number "))
+
+# for i in range(1,11):
+#     print(number ,"*" ,i ,"=", i*number)
+    
+
+#task5    
+sum=0
+number=5
+while(number == 0):
+     
+     print(number)
+     sum=sum + number
+     number=number-1
+print(sum)
+
+#medium
+
+#3
+
+num=5
+fact=1
+i=1
+
+while i <= num:
+    fact*=i
+    i+=1
+print(fact)
